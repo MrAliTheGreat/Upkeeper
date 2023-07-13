632e958f49047bfe11abaa5303739c8323d4a7e7
Ali Bahari <alibahari007@gmail.com>
2023-07-13 22:46:38 +0330

    Add continuous upkeeping

--- emailHandler.py
+++ emailHandler.py
@@ -1,25 +1,26 @@
 import yagmail
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 
 def sendInfo(ip, port, command):
-    print("-   Emailing New Info...")
+    print(f"{datetime.now()} -   Emailing New Info...")
     yag = yagmail.SMTP(os.environ.get("COMEONMAN_EMAIL"), os.environ.get("COMEONMAN_PASSWORD"))
     yag.send(
-        to = os.environ.get("RECEIVER_EMAIL"),
+        to = [os.environ.get("RECEIVER_EMAIL_1"), os.environ.get("RECEIVER_EMAIL_2"), os.environ.get("RECEIVER_EMAIL_3")],
         subject = "Server Updated - New Address",
         contents = getEmailContent(ip, port, command)
     )
-    print("-   Email Sent!")
+    print(f"{datetime.now()} -   Email Sent!")
 
 def getEmailContent(ip, port, command):
     return f"""
         <div style="font-family: sans-serif; background-color: #10171E; color: #FFFFFF; align-items: center; text-align: center;">
             <img src="{os.environ.get("IMAGE_LINK")}" style="height: 200px; width: 200px; border-radius: 50%; margin: 20px;"/>
-            <div style="font-weight: bolder; font-size: 30px;">
+            <div style="font-weight: bolder; font-size: 26px;">
                 New Address Has Been Published!
             </div>
             <div style="font-weight: bolder; font-size: 22px; margin-top: 30px;">
--- upKeeper.py
+++ upKeeper.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from time import sleep
+from time import sleep, time
+from datetime import datetime
 import os
 from dotenv import load_dotenv
 
@@ -13,17 +14,17 @@ load_dotenv()
 
 
 def logout(chrome: webdriver):
-    print("-   Logging Out...")
+    print(f"{datetime.now()} -   Logging Out...")
     chrome.find_element(
         By.CSS_SELECTOR, "span[style='display: table-cell; vertical-align: middle; white-space: nowrap;']"
     ).click()
     chrome.find_element(
         By.CSS_SELECTOR, "a[href='/auth/logout?redirect=%2F']"
     ).click()
-    print("-   Logged Out!")
+    print(f"{datetime.now()} -   Logged Out!")
 
 def login(chrome: webdriver):
-    print("-   Logging In...")
+    print(f"{datetime.now()} -   Logging In...")
     email = chrome.find_element(
         By.ID, "emailInput"
     )
@@ -36,12 +37,14 @@ def login(chrome: webdriver):
     chrome.find_element(
         By.CSS_SELECTOR, "span[class='_1lMForvxbNw3au4JG5jdB9']"
     ).click()
-    print("-   Logged In!")
+    print(f"{datetime.now()} -   Logged In!")
 
 def renewServer(chrome: webdriver):
-    print("-   Renewing Server...")    
-    chrome.find_element(
-        By.CSS_SELECTOR, "a[data-mkt-id='console_main_button_runContainer']"
+    print(f"{datetime.now()} -   Renewing Server...")
+    WebDriverWait(chrome, 20).until(
+        EC.visibility_of_element_located((
+            By.ID, os.environ.get("BUTTON_ID")
+        ))
     ).click()
     chrome.switch_to.window(chrome.window_handles[1])
     sleep(7 * 60)
@@ -51,8 +54,10 @@ def renewServer(chrome: webdriver):
     chrome.refresh()
     sleep(10)
 
-    chrome.find_element(
-        By.CSS_SELECTOR, "button[data-mkt-id='console_main_button_viewContainerMore']"
+    WebDriverWait(chrome, 20).until(
+        EC.visibility_of_element_located((
+            By.CSS_SELECTOR, "button[data-mkt-id='console_main_button_viewContainerMore']"
+        ))
     ).click()
     chrome.find_element(
         By.CSS_SELECTOR, f"a[href='{os.environ.get('SETTING_HREF')}']"
@@ -71,20 +76,31 @@ def renewServer(chrome: webdriver):
         if(cell.text):
             details.append(cell.text)
 
-    print("-   Server Renewed!")
+    print(f"{datetime.now()} -   Server Renewed!")
     return details
 
 
 
-chrome = webdriver.Chrome("./chromedriver.exe")
-chrome.maximize_window()
-chrome.implicitly_wait(5)
-chrome.get(os.environ.get("STARTING_URL"))
-login(chrome)
-sleep(10)
-info = renewServer(chrome)
-sleep(10)
-sendInfo(info[2], info[3], info[4])
-logout(chrome)
-sleep(10)
-chrome.close()
+startTime = time()
+waitPeriodInMinutes = 4 * 60
+
+while(True):
+    chrome = webdriver.Chrome("./chromedriver.exe")
+    chrome.maximize_window()
+    chrome.implicitly_wait(5)
+    
+    chrome.get(os.environ.get("STARTING_URL"))
+
+    login(chrome)
+    sleep(10)
+
+    info = renewServer(chrome)
+    sleep(10)
+    sendInfo(info[2], info[3], info[4])
+
+    logout(chrome)
+    sleep(10)
+    
+    chrome.close()
+    print("==================================================")
+    sleep( (waitPeriodInMinutes * 60.0) - ((time() - startTime) % (waitPeriodInMinutes * 60.0)) )
